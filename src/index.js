983aef28cd5d871ff070d748603ce640231c81a9
maryamS007 <maryam.seif.codes@gmail.com>
2022-08-13 13:19:05 +0430

    edited html, added icon in js

--- src/index.js
+++ src/index.js
@@ -1,96 +1,3 @@
-// let weather = {
-//   paris: {
-//     temp: 19.7,
-//     humidity: 80,
-//   },
-//   tokyo: {
-//     temp: 17.3,
-//     humidity: 50,
-//   },
-//   lisbon: {
-//     temp: 30.2,
-//     humidity: 20,
-//   },
-//   "san francisco": {
-//     temp: 20.9,
-//     humidity: 100,
-//   },
-//   oslo: {
-//     temp: -5,
-//     humidity: 20,
-//   },
-// };
-
-// let city = prompt("Enter city here?");
-// if (city !== undefined) {
-//   let humidity = weather[city].humidity;
-//   let temperature = weather[city].temp;
-//   let celciusTeperature = Math.round(temperature);
-//   let farenheitTemperature = Math.round((temperature * 9) / 5 + 32);
-//   alert(
-//     `It is currently ${celciusTeperature}°C (${farenheitTemperature}°F) in ${city} with the humidity of ${humidity}%.`
-//   );
-// } else {
-//   alert(
-//     `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city} `
-//   );
-// }
-
-// weather.paris.farenheit = 68;
-// weather.tokyo.farenheit = 62.6;
-// weather.lisbon.farenheit = 86;
-// weather["san francisco"].farenheit = 69.8;
-// weather.oslo.farenheit = 23;
-
-// weather.paris.temp = Math.round(weather.paris.temp);
-// weather.tokyo.temp = Math.round(weather.tokyo.temp);
-// weather.lisbon.temp = Math.round(weather.lisbon.temp);
-// weather["san francisco"].temp = Math.round(weather["san francisco"].temp);
-// weather.tokyo.farenheit = Math.round(weather.tokyo.farenheit);
-// weather["san francisco"].farenheit = Math.round(
-//   weather["san francisco"].farenheit
-// );
-
-// let city = prompt("Enter city here");
-// city = city.toLocaleLowerCase().trim();
-
-// if (city === "paris") {
-//   alert(
-//     `It is currently ${weather.paris.temp} °C (${weather.paris.farenheit}°F) in ${city} with a humidity of ${weather.paris.humidity}%.`
-//   );
-// } else {
-//   if (city === "oslo") {
-//     alert(
-//       `It is currently ${weather.oslo.temp} °C (${weather.oslo.farenheit}°F) in ${city} with a humidity of ${weather.oslo.humidity}%.`
-//     );
-//   } else {
-//     if (city === "tokyo") {
-//       alert(
-//         `It is currently ${weather.tokyo.temp} °C (${weather.tokyo.farenheit}°F) in ${city} with a humidity of ${weather.tokyo.humidity}%.`
-//       );
-//     } else {
-//       if (city === "lisbon") {
-//         alert(
-//           `It is currently ${weather.lisbon.temp} °C (${weather.lisbon.farenheit}°F) in ${city} with a humidity of ${weather.lisbon.humidity}%.`
-//         );
-//       } else {
-//         if (city === "san francisco") {
-//           alert(
-//             `It is currently ${weather["san francisco"].temp} °C (${weather["san francisco"].farenheit}°F) in ${city} with a humidity of ${weather["san francisco"].humidity}%.`
-//           );
-//         } else {
-//           if (city === "") {
-//             alert("Dont be shy🙈! look up a city!");
-//           } else {
-//             alert(
-//               `Sorry, we don't know the weather of this city, try going to https://google.com/search?q=weather+${city}.`
-//             );
-//           }
-//         }
-//       }
-//     }
-//   }
-// }
 function formatDate(timestamp) {
   let now = new Date(timestamp);
   let hours = now.getHours();
@@ -113,27 +20,6 @@ function formatDate(timestamp) {
   return `${weekDays[now.getDay()]}, ${hours}:${minutes} `;
 }
 
-// function showTempC(event) {
-//   event.preventDefault();
-//   let tempC = document.querySelector("#main-temp");
-//   let celciusTemperature = 10;
-//   tempC.innerHTML = celciusTemperature;
-// }
-
-// function showTempF(event) {
-//   event.preventDefault();
-//   let tempF = document.querySelector("#main-temp");
-//   let celciusTemperature = 10;
-//   let farenheitTemperature = Math.round((celciusTemperature * 9) / 5 + 32);
-//   tempF.innerHTML = farenheitTemperature;
-// }
-// let tempC = document.querySelector("#celsius-link");
-// tempC.addEventListener("click", showTempC);
-
-// let tempF = document.querySelector("#farenheit-link");
-// tempF.addEventListener("click", showTempF);
-
-// default city
 function showDefaultCity(response) {
   let defaultCity = document.querySelector("#main-city");
   let city = response.data.name;
@@ -158,7 +44,6 @@ let apiUrl = `${apiProtocal}q=${city}&appid=${apiKey}&units=${units}`;
 
 axios.get(apiUrl).then(showDefaultCity);
 
-// homework 1
 function showTypedCityTemp(response) {
   let tempElement = document.querySelector("#main-temp");
   let dafuatlTempC = Math.round(response.data.main.temp);
@@ -171,6 +56,11 @@ function showTypedCityTemp(response) {
   defaulWind.innerHTML = wind;
   let currentTime = document.querySelector("#day-time");
   currentTime.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 function showTypedCity(event) {
   event.preventDefault();
@@ -188,7 +78,6 @@ function showTypedCity(event) {
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", showTypedCity);
 
-// Bosun Challenge
 function showMainTemp(response) {
   let tempElement = response.data.main.temp;
   let mainTemperature = Math.round(tempElement);
